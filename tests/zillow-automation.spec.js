20090db89f4a72178b714743d59625e71cf12ee3
LuisM-Hernandez <m.luishernandez@outlook.com>
2023-11-17 17:00:38 -0500

    added functionality to iterate through all the listings and save the ones with the zip provided. now to test with filters

--- tests/zillow-automation.spec.js
+++ tests/zillow-automation.spec.js
@@ -2,9 +2,12 @@
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
+  //This handles the "Timeout Exceeded" error and helps execute the while loop as it goes through each iteration
+  test.setTimeout(200000)
+
   //Opens zillow and log the user in
   await page.goto('https://www.zillow.com');
-  await page.waitForTimeout(2000);
+  // await page.waitForTimeout(2000);
   // await page.pause();
   // //It closes the google popup
   await page.frameLocator('iframe[title="Sign in with Google Dialog"]').getByLabel('Close').click();
@@ -13,50 +16,64 @@ test('has title', async ({ page }) => {
   //Add your email and password to the fill strings
   // await page.pause();
   await page.getByPlaceholder('Enter email').fill("mr.robot.o@outlook.com");
-  // await page.pause();
   await page.getByPlaceholder('Enter password').fill("RobieOto123!");
   await page.getByRole('button', {name: 'Sign in'}).click();
+  // await page.waitForTimeout(2000);
+  
+  //Searchbar fill
+  await page.getByRole('button', { name: 'Got it!' }).click();
+  await page.getByPlaceholder('Enter an address, neighborhood, city, or ZIP code').fill("32837");
+  // await page.waitForTimeout(2000);
+  await page.getByRole('button', {name: 'Submit Search'}).click();
   await page.waitForTimeout(2000);
-
   // await page.pause();
-  //   await page.getByPlaceholder('Enter an address, neighborhood, city, or ZIP code').fill("Orlando");
+  
+  const ulElement = await page.$$('.List-c11n-8-84-3__sc-1smrmqp-0.StyledSearchListWrapper-srp__sc-1ieen0c-0.doa-doM.fgiidE.photo-cards.photo-cards_extra-attribution li');
+  
+  // Process a limited number of elements in each iteration
+  const elementsPerIteration = 20;
+  let currentIndex = 0;
+  
+  
+  while (currentIndex < ulElement.length) {
+    const endIndex = Math.min(currentIndex + elementsPerIteration, ulElement.length);
+    console.log('Processing elements from index', currentIndex, 'to', endIndex);
 
-  const ulElement = await page.$$('ul[role="list"] li');
-  if (ulElement.length > 0) {
-    // Process each li element in the array
-    for (const liElement of ulElement) {
-      const textContent = await page.evaluate(li => li.textContent, liElement);
 
-      if (textContent?.includes('34746')) {
-        const button = await liElement.$('button');
-        if (button) {
-          await button.click();
-          await page.waitForTimeout(2000);
-          console.log('Text content of li element:', textContent);
+
+
+    for (let i = currentIndex; i < endIndex; i++) {
+      const liElement = ulElement[i];
+      
+      try {
+        const textContent = await page.evaluate(li => li.textContent, liElement);
+  
+        if (textContent?.includes('32837')) {
+          const button = await liElement.$('button');
+
+          if (button) {
+            await page.waitForTimeout(2000);
+
+            await button.click();
+            console.log('Text content of li element:', textContent);
+            
+          }
         }
+      } catch (error) {
+        console.error('Error during processing:', error);
       }
     }
-  } else {
-    console.log('No li elements found inside the ul with role="list".');
+    
+    // Update the current index for the next iteration
+    currentIndex = endIndex;
+    console.log('Finished processing elements from index', currentIndex, 'to', endIndex);
+
+    // Introduce a delay between iterations to avoid overloading the website
+    // await new Promise(resolve => setTimeout(resolve, 2000));
   }
 
-//     //Searchbar fill
-//     // await page.getByRole('button', { name: 'Got it!' }).click();
-//     await page.getByPlaceholder('Enter an address, neighborhood, city, or ZIP code').fill("Orlando");
-//     // await page.pause();
-//     await page.getByRole('button', {name: 'Submit Search'}).click();
-  
-  
-//     // Filters
-  
-//     // Filter Bed and Baths
-//    await page.getByRole('button', { name: 'Beds & Baths' }).click();
-//    await page.getByLabel('Beds Select').getByRole('button', { name: '4+' }).click();
-//    await page.getByLabel('Baths Select').getByRole('button', { name: '2+' }).click();
-//  await page.locator('[data-test="close-filters-button"]').click();
-// // Filter Price
-//  await page.locator('[data-test="price-filters-button"]').click();
-//  await page.getByLabel('Price max').fill("400,000");
-//  await page.getByRole('button', {name: 'Apply'}).click();
+  });
 
-});
+
+
+
