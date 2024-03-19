d2f954d60ecfbc9dfd1305558ecef833db81e663
LuisM-Hernandez <m.luishernandez@outlook.com>
2024-03-19 10:01:05 -0400

    Fixed time issue when writing the city. Fixed list locator

--- tests/zillow-automation.spec.js
+++ tests/zillow-automation.spec.js
@@ -14,49 +14,49 @@ test('has title', async ({ page }) => {
   const maxPriceRange = "ENTER MAX PRICE RANGE IN THIS FORMAT (400,000)"
   const zipCode = "ENTER THE ZIPCODE"
   
+  await page.waitForTimeout(2000);
   //Opens zillow and log the user in
   await page.goto('https://www.zillow.com/');
-  // await page.pause();
   
   //It closes the google popup
   await page.frameLocator('iframe[title="Sign in with Google Dialog"]').getByLabel('Close').click();
   await page.getByRole('link', { name: 'Sign In' }).click();
   
   //Add your email and password to the fill the fields
-
+  
   await page.getByPlaceholder('Enter email').fill(email);
   await page.getByPlaceholder('Enter password').fill(password);
   await page.getByRole('button', {name: 'Sign in'}).click();
   
+  // await page.pause();
   //Searchbar fill
-  await page.getByRole('button', { name: 'Got it!' }).click();
+  await page.getByPlaceholder('Enter an address,').click();
+  await page.waitForTimeout(2000);
   await page.getByPlaceholder('Enter an address, neighborhood, city, or ZIP code').fill(city);
-  // await page.waitForTimeout(2000);
+  await page.waitForTimeout(2000);
   await page.getByRole('button', {name: 'Submit Search'}).click();
   //This timeout is set so the test keeps running
   await page.waitForTimeout(2000);
-
-  // Filters
+    
   
-
   // Filter Bed and Baths
   await page.getByRole('button', { name: 'Beds & Baths' }).click();
   await page.getByLabel('Beds Select').getByRole('button', { name: bedAmount }).click();
   await page.getByLabel('Baths Select').getByRole('button', { name: bathAmount }).click();
   await page.locator('[data-test="close-filters-button"]').click();
-
+  
   // Filter Price
-   await page.locator('[data-test="price-filters-button"]').click();
-   await page.getByLabel('Price max').fill(maxPriceRange);
-   await page.getByRole('button', {name: 'Apply'}).click();
-     //This timeout is set so the test keeps running
+  await page.locator('[data-test="price-filters-button"]').click();
+  await page.getByLabel('Price max').fill(maxPriceRange);
+  await page.getByRole('button', {name: 'Apply'}).click();
+  //This timeout is set so the test keeps running
   await page.waitForTimeout(2000);
-
+  
 
   const liCount = await page.$$eval('.List-c11n-8-84-3__sc-1smrmqp-0 > li', lis => lis.length);
 
   // Get all li elements within the UL
-  const liElements = await page.$$('.List-c11n-8-84-3__sc-1smrmqp-0 li');
+  const liElements = await page.$$('.List-c11n-8-100-1__sc-1smrmqp-0 li');
 
 
   // Iterate through each li element
